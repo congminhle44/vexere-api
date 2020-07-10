d7a27138101a19fcc8c63d15a9d0dbbdbc7c4760
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-07-10 18:42:12 +0700

    first commit

---
+++ models/Trip.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { SeatSchema } = require("./Seat");
+
+const TripSchema = new mongoose.Schema({
+  fromStationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Station",
+  },
+  toStationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Station",
+  },
+  startTime: {
+    type: Date,
+    default: new Date(),
+  },
+  seats: [SeatSchema],
+  price: { type: Number, default: 0 },
+});
+
+const Trip = mongoose.model("Trip", TripSchema, "Trip");
+
+module.exports = {
+  TripSchema,
+  Trip,
+};
---
+++ services/email/bookTicket.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs"); // built-in NodeJS (NodeAPI)
+const hogan = require("hogan.js");
+
+const template = fs.readFileSync(
+  "services/email/bookingTicketEmailTemplate.hjs",
+  "utf-8"
+);
+const compiledTemplate = hogan.compile(template);
+
+module.exports.sendBookTicketEmail = (user, trip, ticket) => {
+  const transport = {
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    requireSSL: true,
+    auth: {
+      user: "lucas.lee752000@gmail.com",
+      pass: "Minh123456",
+    },
+  };
+
+  const transporter = nodemailer.createTransport(transport);
+
+  const mailOptions = {
+    from: "lucas.lee752000@gmail.com",
+    to: "congminhle44@gmail.com",
+    subject: "Mail xac nhan mua ve thanh cong",
+    html: compiledTemplate.render({
+      email: user.email, // lay tu user
+      fromStation: trip.fromStationId.name,
+      toStation: trip.toStationId.name,
+      price: trip.price,
+      amount: ticket.seats.length,
+      total: trip.price * ticket.seats.length,
+      seatCodes: ticket.seats.map((m) => m.code).join(", "),
+    }),
+  };
+
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) return console.log(err);
+    console.log("Send email successfully");
+  });
+};
