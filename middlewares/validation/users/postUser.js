60973862d22d48ec7d8077fef68193b501768ef6
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-09-06 12:37:29 +0700

    commit

--- middlewares/validation/users/postUser.js
+++ middlewares/validation/users/postUser.js
@@ -1,9 +1,9 @@
 const validator = require("validator");
 const _ = require("lodash");
-const { User } = require("../../../models/User")
+const { User } = require("../../../models/User");
 
 const validatePostUser = async (req, res, next) => {
-  let errors = {}
+  let errors = {};
 
   const email = _.get(req, "body.email", "");
   const password = _.get(req, "body.password", "");
@@ -14,37 +14,37 @@ const validatePostUser = async (req, res, next) => {
   // validate email
   // email empty?
   if (validator.isEmpty(email)) {
-    errors.email = "Email is required"
+    errors.email = "Email is required";
   } else if (!validator.isEmail(email)) {
-    errors.email = "Email is invalid"
+    errors.email = "Email is invalid";
   } else {
-    const user = await User.findOne({ email })
-    if (user) errors.email = "Email exists"
+    const user = await User.findOne({ email });
+    if (user) errors.email = "Email exists";
   }
 
   // validate password
   if (validator.isEmpty(password)) {
-    errors.password = "Password is required"
+    errors.password = "Password is required";
   } else if (!validator.isLength(password, { min: 6 })) {
-    errors.password = "Password must have at least 6 characters"
+    errors.password = "Password must have at least 6 characters";
   }
 
   // validate password 2
   if (validator.isEmpty(password2)) {
-    errors.password2 = "Password2 is required"
+    errors.password2 = "Password2 is required";
   } else if (!validator.equals(password, password2)) {
-    errors.password2 = "Confirm password must match"
+    errors.password2 = "Confirm password must match";
   }
 
   // fullName
   if (validator.isEmpty(fullName)) {
-    errors.fullName = "Full name is required"
+    errors.fullName = "Full name is required";
   }
 
   if (_.isEmpty(errors)) return next(); // pass validate
-  return res.status(400).json(errors)
-}
+  return res.status(400).json(errors);
+};
 
 module.exports = {
-  validatePostUser
-}
+  validatePostUser,
+};
--- models/User.js
+++ models/User.js
@@ -6,23 +6,25 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   fullName: { type: String, required: true },
   userType: { type: String, default: "client" },
-  avatar: { type: String }
-})
+  avatar: { type: String },
+});
 
+// Trước khi đăng ký thì hash mật khẩu
 UserSchema.pre("save", function beforeSave(next) {
   const user = this;
   if (!user.isModified("password")) return next();
-  bcrypt.genSalt(10)
-    .then(salt => bcrypt.hash(user.password, salt))
-    .then(hash => {
+  bcrypt
+    .genSalt(10) // Độ khó của chuỗi muối
+    .then((salt) => bcrypt.hash(user.password, salt)) //Bắt đầu encrypt cũng với chuỗi muối
+    .then((hash) => {
       user.password = hash;
       next();
-    })
-})
+    });
+});
 
 const User = mongoose.model("User", UserSchema, "User");
 
-
 module.exports = {
-  UserSchema, User
-}
+  UserSchema,
+  User,
+};
